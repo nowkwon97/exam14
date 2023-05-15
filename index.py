56ca4a534417816417275f340a613258a9906ebf
leejaekwon <nowkwon97@gmail.com>
2023-05-15 21:09:09 +0900

    python 파일 작성

---
+++ index.py
@@ -0,0 +1,14 @@
+# 파이썬에서는 typeof 대신 isinstance() 함수를 사용하여
+# 변수의 유형 확인 가능
+# 예외를 발생시키기 위해 throw 대신 raise 사용
+# 파이썬에서는 Math.floor() 대신 init()함수를 사용하여
+# 소수점 이하 제거 가능
+
+# 파이썬에서는 function 대신 def 사용
+def example_one(a):
+    if not isinstance(a, int):
+        raise ValueError('정수를 입력해야 합니다.')
+
+    return a
+
+print(example_one(4))

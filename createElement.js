1ee8a231890ef468b664a07bfc6607584b747ca8
leejaekwon <nowkwon97@gmail.com>
2023-05-15 23:21:36 +0900

    주석 추가

--- createElement.js
+++ createElement.js
@@ -1,3 +1,15 @@
+/**
+ * ? 엘리먼트를 생성하는 함수
+ * @param {string} tagName 
+ * @param {object} props 
+ * @returns {string}
+ * * 첫번째 매개변수 tagName은 문자열이여야 한다.
+ * * 두번째 매개변수 props는 객체여야 한다.
+ * * 리턴값은 문자열이다.
+ * 
+ * * 아래의 함수의 안정성을 위해 타입스크립트로 작성해보자.
+ */
+
 function createElement(tagName, props) {
   let propsString = "";
   for (let key in props) {
